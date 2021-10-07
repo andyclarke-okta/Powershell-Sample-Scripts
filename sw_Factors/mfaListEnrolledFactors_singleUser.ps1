913f712bb4634eab79e45a1dbf5bbdf81de338eb
Andy Clarke <andy.clarke@okta.com>
2021-10-07 15:18:37 -0400

    added Okta User Factor scripts

---
+++ sw_Factors/mfaListEnrolledFactors_singleUser.ps1
@@ -0,0 +1,37 @@
+﻿#list enrolled factors single user
+#site config
+$org = "https://xxxxxxxxxxxxx.okta.com"
+$token="xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxx"
+$basicAuthValue = "SSWS $token"
+$headers = @{}
+$headers["Authorization"] = $basicAuthValue
+$headers["Accept"] = "application/json"
+$headers["Content-Type"] = "application/json"
+
+#API parameters
+$user = "test.testuser1@mydomain.com"
+$uri1 = "$org/api/v1/users/" + $user
+
+$result1 = Invoke-WebRequest -Uri $uri1 -Headers $headers -Method GET
+#NOTE: this provides all response header fields
+$result1
+#NOTE: this provides body payload
+$rContent1 = $result1.Content | ConvertFrom-json
+$rContent1
+#$rContent.credentials
+$rContent1._links
+$rContent1.profile
+$OktaId = $rContent1.id
+
+##############################################
+#API parameters
+$uri2 = "$org/api/v1/users/" + $OktaId + "/factors"
+
+$result2 = Invoke-WebRequest -Uri $uri2 -Headers $headers -Method GET
+$result2.StatusCode
+#$result2
+#NOTE: this provides all response header fields
+$rContent2 = $result2.Content | ConvertFrom-json 
+$rContent2 | Get-Member
+$rContent2[0]
+$rContent2[1]
---
+++ sw_Factors/mfaResetFactors_singleUser.ps1
@@ -0,0 +1,37 @@
+﻿#reset all MFA factors for single user
+#site config
+$org = "https://xxxxxxxxxxx.oktapreview.com"
+$token="xxxxxxxxxxxxxxxxxxxx"
+$basicAuthValue = "SSWS $token"
+$headers = @{}
+$headers["Authorization"] = $basicAuthValue
+$headers["Accept"] = "application/json"
+$headers["Content-Type"] = "application/json"
+
+
+#API parameters
+$user = "test.testuser1@mydomain.com"
+$uri1 = "$org/api/v1/users/" + $user
+
+$result1 = Invoke-WebRequest -Uri $uri1 -Headers $headers -Method GET
+#NOTE: this provides all response header fields
+$result1
+#NOTE: this provides body payload
+$rContent1 = $result1.Content | ConvertFrom-json
+$rContent1
+#$rContent.credentials
+$rContent1._links
+$rContent1.profile
+$OktaId = $rContent1.id
+
+######################################################
+#API parameters
+
+$uri2 = "$org/api/v1/users/" + $OktaId + "/lifecycle/reset_factors"
+$result2 = Invoke-WebRequest -Uri $uri2 -Headers $headers -Method POST
+#NOTE: this provides all response header fields
+$result2
+#NOTE: this provides body payload
+$rcContent2 = $result2.Content | ConvertFrom-json
+$rContent2
+$rContent2.credentials

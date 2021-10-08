3f4b8027a5e04ce116ccb4735c6a1fa2f78d7a08
Andy Clarke <andy.clarke@okta.com>
2021-10-08 16:35:01 -0400

    added Misc utility scripts

---
+++ sw_misc/Convert_PasswordToHash.ps1
@@ -0,0 +1,29 @@
+﻿#Here is a simple script to hash a string using your chosen cryptography algorithm.
+#Usage Examples:
+#Get-StringHash "My String to hash" "MD5"
+#Get-StringHash "My String to hash" "RIPEMD160"
+#Get-StringHash "My String to hash" "SHA1"
+#Get-StringHash "My String to hash" "SHA256"
+#Get-StringHash "My String to hash" "SHA384"
+#Get-StringHash "My String to hash" "SHA512"
+
+#http://jongurgul.com/blog/#Get-StringHash-get-filehash/
+#https://gallery.technet.microsoft.com/scriptcenter/Get-StringHash-aa843f71
+
+##set hash algorithm
+$algo = 'SHA256';
+
+
+
+
+Function Get-StringHash([String] $String,$HashName)
+{
+$StringBuilder = New-Object System.Text.StringBuilder
+[System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String))|%{
+[Void]$StringBuilder.Append($_.ToString("x2"))
+}
+$StringBuilder.ToString()
+}
+
+$myvar = Read-Host 'Enter string to convert to hash using ' $algo 'algorithm'
+Get-StringHash $myvar $algo
---
+++ sw_misc/create_testUser_csvFiles.ps1
@@ -0,0 +1,27 @@
+﻿
+#config home folder
+$homeFolder = "C:\OktaUsers\TestUsers";
+
+#setup output file headers
+$myFile = "$homeFolder\Test_User_import_100.csv";
+"uid,givenname,sn,mail," |Out-File $myFile
+
+
+write-host "script start " (get-date).ToString()
+
+
+
+For ($x=1; $x -le 100; $x++)
+{
+
+$A = "test.okta" + $x + "@mailinator.com";
+$B = "okta" + $x;
+$C = "oktatest"+ $x;
+$D = "test.okta" + $x +  "@mailinator.com";
+
+$wrapper = New-Object PSObject -Property @{ uid = $A; givenname = $B;sn =$C;mail=$D }
+Export-Csv -InputObject $wrapper -Path $myFile -Append -Force
+}
+
+
+write-host "script end " (get-date).ToString()
